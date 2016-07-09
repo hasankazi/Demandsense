e3ce7cfb69a5576de6bde1853b457e2c0eaf7c16
hasan kazi <hasankazi@gmail.com>
2016-07-09 15:08:14 -0400

    implemented devise for user1s+++3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 #required for heroku
- config.action_mailer.default_url_options = { host: 'www.demandsense.com',end
+ config.action_mailer.default_url_options = { host: 'www.demandsense.com' }
